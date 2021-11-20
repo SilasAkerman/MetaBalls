091e570261ebd675b4fbc6381a9ff140dc276b33
SilasAkerman <94759651+SilasAkerman@users.noreply.github.com>
2021-11-20 20:26:26 +0100

    Add files via upload

---
+++ Blob.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+using Raylib_cs;
+
+namespace MetaBalls
+{
+    class Blob
+    {
+        const int speed = 4;
+
+        Vector2 pos = Vector2.Zero;
+        Vector2 vel = Vector2.Zero;
+        int size;
+
+        Random rand = new Random();
+
+        public Blob()
+        {
+            pos.X = rand.Next(Program.WIDTH);
+            pos.Y = rand.Next(Program.HEIGHT);
+
+            vel.X = (float)rand.NextDouble() * 2 * speed - speed;
+            vel.Y = (float)rand.NextDouble() * 2 - 1;
+
+            size = rand.Next(5, 25);
+        }
+
+        public double CalculateMetaValue(Vector2 pixelPos)
+        {
+            double distance = Vector2.Distance(pixelPos, pos);
+            return size / distance;
+        }
+
+        public void Display()
+        {
+            Raylib.DrawCircleLines((int)pos.X, (int)pos.Y, size, Color.BLACK);
+        }
+
+        public void Update()
+        {
+            pos += vel;
+            edges();
+        }
+        private void edges()
+        {
+            if (pos.X + size > Program.WIDTH || pos.X - size < 0)
+            {
+                vel.X *= -1;
+            }
+            if (pos.Y + size > Program.HEIGHT || pos.Y - size < 0)
+            {
+                vel.Y *= -1;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Numerics;
+using Raylib_cs;
+
+namespace MetaBalls
+{
+    class Program
+    {
+        public const int WIDTH = 400;
+        public const int HEIGHT = 400;
+        const int SPACING = 1;
+
+        const int ROWS = HEIGHT / SPACING;
+        const int COLS = WIDTH / SPACING;
+
+        const int metaScalar = 2000;
+
+        static Blob[] blobs = new Blob[6];
+
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(WIDTH, HEIGHT, "MetaBalls");
+            Raylib.SetTargetFPS(60);
+
+            for (int i = 0; i < blobs.Length; i++)
+            {
+                blobs[i] = new Blob();
+            }
+
+            while (!Raylib.WindowShouldClose())
+            {
+                foreach (Blob blob in blobs)
+                {
+                    blob.Update();
+                }
+
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.RAYWHITE);
+
+                for (int x = 0; x < COLS; x++)
+                {
+                    for (int y = 0; y < ROWS; y++)
+                    {
+                        Vector2 pixelPos = new Vector2(x * SPACING, y * SPACING);
+                        double total = 0;
+                        foreach(Blob blob in blobs)
+                        {
+                            total += blob.CalculateMetaValue(pixelPos) * metaScalar / blobs.Length;
+                        }
+                        //Color color = Raylib.ColorFromHSV((int)Math.Clamp(total, 0, 255), 1, 1);
+                        Color color = Raylib.ColorFromHSV((int)total, 1, 1);
+                        Raylib.DrawPixelV(pixelPos, color);
+                        //Raylib.DrawRectangle((int)pixelPos.X, (int)pixelPos.Y, SPACING, SPACING, color);
+                    }
+                }
+
+                //foreach (Blob blob in blobs)
+                //{
+                //    blob.Display();
+                //}
+                Raylib.DrawFPS(50, 50);
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
